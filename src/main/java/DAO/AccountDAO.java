bcc3db62593d03693c9b37deda4a4611532f50d3
Shkoh Hamasoor <shkoh.hamasoor@gmail.com>
2023-02-06 01:20:52 +0000

    Created an accountDAO thats functional

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -1,7 +1,39 @@
 package DAO;
 
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import Model.Account;
+import Util.ConnectionUtil;
+
 public class AccountDAO {
     /**
-     * retrieve all 
+     * new users
      */
+    public Account addNewAccount(Account account){
+        Connection connection = ConnectionUtil.getConnection();
+        try{
+            //sql logic here
+            String sql = "INSERT INTO account (username, password) VALUES (?,?)";
+            //Statement.RETURN_GENERATED_KEYS
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            //write preparedstatement logic here
+            preparedStatement.setString(1, account.getUsername());
+            preparedStatement.setString(2,account.getPassword());
+
+            preparedStatement.executeUpdate();
+            ResultSet rs = preparedStatement.getGeneratedKeys();
+            if(rs.next()){
+                int generated_account_id = (int) rs.getLong(1);
+                return new Account(generated_account_id, account.getUsername(), account.getPassword());
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 }
--- src/main/java/DAO/todo.md
+++ src/main/java/DAO/todo.md
@@ -1,11 +1,2 @@
 You will need to design and create your own DAO classes from scratch. 
 You should refer to prior mini-project lab examples and course material for guidance.
-
-
-//test
-
-
-//doing a second test
-
-
-//doing a thrid test
